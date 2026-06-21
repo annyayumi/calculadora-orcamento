97dc3d9547597fb823d990fce0e94fbb37528152
Anny Ayumi <anny.iogi@ammya.com.br>
2026-06-21 00:02:29 -0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintPluginPrettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  eslintPluginPrettier, // Sempre por último para sobrescrever conflitos entre o ESlint e o Prettier.
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/src/components/ui/button';
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-background text-foreground">
+            <div className="text-center space-y-4">
+                <h1 className="text-4xl font-bold tracking-tight">
+                    Next.js 15 + shadcn/ui
+                </h1>
+                <p className="text-muted-foreground">
+                    Ambiente configurado com TypeScript e Tailwind CSS.
+                </p>
+                <Button>Clique aqui</Button>
+            </div>
+        </main>
+    );
+}
